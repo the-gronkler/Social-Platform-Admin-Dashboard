13e22438a4cb65162bd1d8786a456bc6b05690ac
the-gronkler <alexsvirin1337@gmail.com>
2025-01-05 01:23:35 +0100

    pivot table admin status triggers

--- app/Models/UsersServer.php
+++ app/Models/UsersServer.php
@@ -19,4 +19,54 @@ class UsersServer extends Pivot
     ];
 
     public $timestamps = false;
+
+    protected static function booted()
+    {
+        // if the user being deleted is the last admin, the earliest joined user will be made an admin
+        static::deleting(function (UsersServer $usersServer) {
+            $server = Server::find($usersServer->server_id);
+            if (!$server || $server->users()->isEmpty()) {
+                return;
+            }
+
+            $serverHas1Admin = $server->users()
+                    ->wherePivot('is_admin', true)
+                    ->count() == 1;
+
+            if ($usersServer->is_admin && $serverHas1Admin) {
+                $earliestJoinedUser = $server->users()->orderBy('users_server.created_at')->first();
+
+                \DB::transaction(function () use ($server, $earliestJoinedUser) {
+                    $server->users()->updateExistingPivot($earliestJoinedUser->id, ['is_admin' => true]);
+                });
+            }
+        });
+
+        // if the server has no users, the first user to join will be an admin
+        static::creating(function (UsersServer $usersServer) {
+            $server = Server::find($usersServer->server_id);
+            if (!$server)
+                return;
+
+            if ($server->users()->count() == 0)
+                $usersServer->is_admin = true;
+        });
+
+        // if the update is removing the last admin's priveleges, the earliest joined user will be made an admin
+        static::updated(function (UsersServer $usersServer) {
+            $server = Server::find($usersServer->server_id);
+            if (!$server)
+                return;
+
+            // Check if there are no admins on the server
+            if ( $server->users()->wherePivot('is_admin', true)->count() > 0)
+                return;
+
+            $earliestJoinedUser = $server->users()->orderBy('users_server.created_at')->first();
+
+            \DB::transaction(function () use ($server, $earliestJoinedUser) {
+                $server->users()->updateExistingPivot($earliestJoinedUser->id, ['is_admin' => true]);
+            });
+        });
+    }
 }
